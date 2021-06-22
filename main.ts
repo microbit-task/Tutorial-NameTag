230ca61f7849b4022f46db00f5ffc2533ca32aaf
tampopo <67646094+tampopo256@users.noreply.github.com>
2021-06-22 22:58:23 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("last name")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("first name")
+})
